8223e0ebf0cfa4f248a4b534dd83b1b70fd1b8f4
Rodney Dabre <128955508+dabrerodney@users.noreply.github.com>
2024-02-12 23:49:51 +0530

    2

---
+++ UT1 Programs/chp1q6.php
@@ -0,0 +1,7 @@
+<?php
+$i = 1;
+do {
+    echo $i;
+    echo "<br>";
+    $i += 2;
+} while ($i < 10);
---
+++ UT1 Programs/edOfDoWhileLoop.php
@@ -0,0 +1,8 @@
+<?php
+$i = 2;
+do {
+    echo $i;
+    echo "<br>";
+    $i += 2;
+} while ($i <= 10);
+?>
